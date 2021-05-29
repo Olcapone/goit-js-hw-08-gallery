abb915176b7462ba4f6c5393e06fe519a9412cea
OLgaPonomarenko <fblackberries@gmail.com>
2021-05-29 20:07:40 +0300

    закрытие по кнопке и по оверлею

--- gallery-dz.js
+++ gallery-dz.js
@@ -7,10 +7,9 @@ const refs = {
    
     galleryEl: document.querySelector('.js-gallery'),
     modalWindow: document.querySelector('.js-lightbox'),
-    modalContent: document.querySelector('.lightbox__content'),
     modalImage: document.querySelector('.lightbox__image'),
-    modalClose: document.querySelector('.lightbox__button')
-
+    modalClose: document.querySelector('.lightbox__button'),
+    modalOverlay: document.querySelector('.lightbox__overlay')
   };
 
   // добавляю картинки
@@ -34,17 +33,24 @@ const refs = {
     refs.modalWindow.classList.add('is-open');
    // refs.modalWindow.style.display = 'block';
     refs.modalImage.src = event.target.dataset.source;
-    console.log( refs.modalImage.src)
+    
   };
 
 // добавляю слушатель на закрытие картинок
 
  refs.modalClose.addEventListener('click', OnClose);
+ refs.modalOverlay.addEventListener('click', OnClose);
+ window.addEventListener('keydown', OnExcClose);
+ 
 
  function OnClose (e){
     refs.modalWindow.classList.remove('is-open');
     refs.modalImage.src = '';
-    console.log( refs.modalImage.src);
- }
+    
+    window.removeEventListener('keydown', OnClose);
+ };
 
+function OnExcClose (e){
+  if(e.code === 'Escape') OnClose();
+}
   
